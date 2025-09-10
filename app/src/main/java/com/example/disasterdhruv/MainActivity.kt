91d2a7ec1283cf9b5d024fef49519589a98e2b85
HimanshuPadam <himanshupadam808@gmail.com>
2025-09-10 21:55:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/disasterdhruv/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.disasterdhruv
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.disasterdhruv.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val webSettings: WebSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://uidai.gov.in/en/")
+    }
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
